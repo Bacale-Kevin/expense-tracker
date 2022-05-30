85a274ac3d99c10f129f1b80e42e2ed28604d2ff
bacale <bacale86@gmail.com>
2022-05-31 00:11:53 +0100

    feat(server): add routes and controller

---
+++ server/controller/controller.js
@@ -0,0 +1,8 @@
+//get categories
+function create_Categories(req, res) {
+  res.json("Get Request from Category");
+}
+
+module.exports = {
+  create_Categories,
+};
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+require('dotenv').config({path: './config.env'})
+
+const port = process.env.PORT || 5000
+
+//use middlewares
+app.use(cors())
+app.use(express.json())
+
+//using routes
+app.use(require('./routes/route'))
+
+app.listen(port, () =>
+  console.log(`Server running on port:http://localhost:${port}`)
+);
