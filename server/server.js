1b2ccd8b8403c2e6ea14483744ee2e5330e1fef2
bacale <bacale86@gmail.com>
2022-05-31 07:13:33 +0100

    feat(DB): connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-require('dotenv').config({path: './config.env'})
+require("dotenv").config({ path: "./config.env" });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //use middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+const con = require("./db/connection");
 
 //using routes
-app.use(require('./routes/route'))
+app.use(require("./routes/route"));
 
-app.listen(port, () =>
-  console.log(`Server running on port:http://localhost:${port}`)
-);
+con.then((db) => {
+  if (!db) return process.exit(1);
+
+  //listen to the http server
+  app.listen(port, () => console.log(`Server running on port:http://localhost:${port}`));
+
+  app.on(`error`, (err) => console.log(`Failed to connect with HTTP Server:${err}`));
+}).catch(err => {
+    console.log(`Connection failed ... ${err}`)
+})
