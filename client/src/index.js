311b273a4ce74349afcd7b4658c744653442c938
bacale <bacale86@gmail.com>
2022-05-31 09:41:48 +0100

    feat(redux): add redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { Provider } from 'react-redux'
+import { store } from "./store/store";
+
+
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Router>
+    <Provider store={store}>
       <App />
-    </Router>
+    </Provider>
 );
