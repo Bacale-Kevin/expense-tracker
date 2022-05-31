e79b14224a2ada6deec3f1aeb179331863436f1d
bacale <bacale86@gmail.com>
2022-05-31 08:40:20 +0100

    feat(server): created CRUD for collections

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,106 @@
-//get categories
-function create_Categories(req, res) {
-  res.json("Get Request from Category");
+const model = require("../models/model");
+
+//create categories
+async function create_Categories(req, res) {
+  const Create = new model.Categories({
+    type: "Investment",
+    color: "#FCBE44",
+  });
+
+  await Create.save(function (err) {
+    if (!err) return res.json(Create);
+
+    return res.status(400).json({ message: `Error while creating categories${err}` });
+  });
+}
+
+//GET Categories
+async function get_categories(req, res) {
+  let data = await model.Categories.find({});
+
+  let filter = await data.map((v) => Object.assign({}, { type: v.type, color: v.color }));
+
+  return res.json(filter);
+}
+
+//POST create Transaction
+async function create_Transaction(req, res) {
+  if (!req.body) return res.status(400).json("Post HTTP Data not provided");
+  const { name, type, amount } = req.body;
+
+  const create = await new model.Transaction({
+    name,
+    type,
+    amount,
+  });
+
+  create.save(function (err) {
+    if (!err) return res.json(create);
+
+    return res.status(400).json({ message: `Error while creating transaction${err}` });
+  });
+}
+
+//GET Transaction
+async function get_Transaction(req, res) {
+  let data = await model.Transaction.find({});
+
+  return res.json(data);
+}
+
+//DELETE
+async function delete_Transaction(req, res) {
+  if (!req.body) res.status(400).json({ message: "Requeest body not Found" });
+
+  await model.Transaction.deleteOne(req.body, function (err) {
+    if (!err) res.json("Record Deleted...!");
+  })
+    .clone()
+    .catch(function (err) {
+      res.json("Error while deleting Transaction Record");
+    });
+}
+
+//GET   aggregate joins two collections together
+async function get_labels(req, res) {
+  model.Transaction.aggregate([
+    {
+      $lookup: {
+        from: "categories", //specify the collection to join
+        localField: "type", // the field or attribute to be join
+        foreignField: "type",
+        as: "categories_info", // the display name of the output array any value can be specified
+      },
+    },
+    {
+      $unwind: "$categories_info",
+    },
+  ])
+    .then((result) => {
+      let data = result.map((v) =>
+        Object.assign(
+          {},
+          {
+            _id: v._id,
+            name: v.name,
+            type: v.type,
+            amount: v.amount,
+            color: v.categories_info["color"],
+          }
+        )
+      );
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json("Lookup Collection Error");
+    });
 }
 
 module.exports = {
   create_Categories,
+  get_categories,
+  create_Transaction,
+  get_Transaction,
+  delete_Transaction,
+  get_labels,
 };
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,14 @@
 const routes = require("express").Router();
 const controller = require("../controller/controller");
 
-routes.route("/api/categories").get(controller.create_Categories);
+routes.route("/api/categories").post(controller.create_Categories).get(controller.get_categories);
+
+routes
+  .route("/api/transaction")
+  .post(controller.create_Transaction)
+  .get(controller.get_Transaction)
+  .delete(controller.delete_Transaction);
+
+routes.route('/api/lables').get(controller.get_labels)
 
 module.exports = routes;
